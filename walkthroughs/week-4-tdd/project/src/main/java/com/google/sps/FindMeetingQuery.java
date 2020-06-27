c001d825c8f40ef20ef726b720e09281e3a6b1e5
Diego <dprado981@gmail.com>
2020-06-27 12:12:55 -0700

    Implement algorithm to find meeting times

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -16,79 +16,101 @@ package com.google.sps;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Arrays;
 import java.util.ArrayList;
 
 public final class FindMeetingQuery {
 
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    // If no attendees, all times work
-    if (request.getAttendees().isEmpty()) {
-      return Arrays.asList(TimeRange.WHOLE_DAY);
-    }
-
     // If a request lasts for longer than the day, no times work
-    if(request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
-      return Arrays.asList();
+    if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+      return Collections.emptyList();
     }
+    
+    Collection<String> requestingAttendees = request.getAttendees();
+    ArrayList<TimeRange> relevantTimes = getRelevantTimes(events, requestingAttendees);
 
+    ArrayList<TimeRange> blockTimes = mergeBlocks(relevantTimes);
+    
+    ArrayList<TimeRange> invertedTimeRanges = invertTimeRanges(blockTimes);
 
-    // Get all event times
-    ArrayList<TimeRange> eventTimeRanges = new ArrayList<>();
-    for(Event event : events.toArray(new Event[0])) {
-      eventTimeRanges.add(event.getWhen());
-    } 
+    ArrayList<TimeRange> possibleTimeRanges = getTimesLongerThan(request.getDuration(), invertedTimeRanges);
 
-    Collections.sort(eventTimeRanges, TimeRange.ORDER_BY_START);
-
-
-    // If single event, invert time
-    if(events.size() == 1) {
-      return invertTimeRange(eventTimeRanges.get(0));
-    } 
-/*
-    // If times overlap, get the start and end of them, then invert
-    System.out.println(hasBlockOverlap(eventTimeRanges));
-    if (hasBlockOverlap(eventTimeRanges)) {
-      return invertTimeRange(mergeTimeRanges(eventTimeRanges));
-    }
-*/
-    return null;
+    return possibleTimeRanges;
   }
 
-  // Returns the TimeRanges that do not include the passed in timeRange
-  private Collection<TimeRange> invertTimeRange(TimeRange timeRange) {
-    TimeRange before = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, timeRange.start(), false);
-    TimeRange after = TimeRange.fromStartEnd(timeRange.end(), TimeRange.END_OF_DAY, true);
-    return Arrays.asList(before, after);
-
-  }
-
-  // Checks if all TimeRanges overlap to form one block
-  private boolean hasBlockOverlap(ArrayList<TimeRange> ranges) {
-    Collections.sort(ranges, TimeRange.ORDER_BY_START);
-    for(int i = 0; i < ranges.size() - 1; i++) {
-      if (!ranges.get(i).overlaps(ranges.get(i+1))) {
-        return false;
+  /** Returns only the TimeRanges that correspond with events that an attendee from requestingAttendees is attending. */
+  private ArrayList<TimeRange> getRelevantTimes(Collection<Event> events, Collection<String> requestingAttendees) {
+    ArrayList<TimeRange> relevantTimes = new ArrayList<>();
+    for (Event event : events) {
+      Collection<String> eventAtendees = event.getAttendees();
+      // Add to relevantTimes if a requesting attendee is in the event
+      for (String requestingAttendee : requestingAttendees) {
+        if (eventAtendees.contains(requestingAttendee)) {
+          relevantTimes.add(event.getWhen());
+        }
       }
     }
-    return true;
+    return relevantTimes;
   }
 
-  // Combines block overlapping TimeRanges into one
-  private TimeRange mergeTimeRanges(Collection<TimeRange> ranges) {
-    int start = TimeRange.START_OF_DAY;
-    int end = TimeRange.END_OF_DAY;
-    for(TimeRange timeRange : ranges) {
-      if (timeRange.start() > start) {
-        start = timeRange.start();
+  /** Returns the result of merging TimeRanges from relevantTimes. */
+  private ArrayList<TimeRange> mergeBlocks(ArrayList<TimeRange> relevantTimes) {
+    // Sort TimeRanges by their start time for easier merging of blocks
+    Collections.sort(relevantTimes, TimeRange.ORDER_BY_START);
+    ArrayList<TimeRange> blockTimes = new ArrayList<>();
+    for (TimeRange time : relevantTimes) {
+      // Check if time with any existing events, if so replace event
+      TimeRange timeToAdd = time;
+      boolean blockTimeAdded = false;
+      for (int i = 0; i < blockTimes.size(); i++) {
+        // Use TimeRange.overlaps(TimeRange) to see if they overlap
+        TimeRange oldEventTime = blockTimes.get(i);
+        TimeRange newEventTime = time;
+        if(oldEventTime.overlaps(newEventTime)) {
+          // Make new TimeRange that spans both TimeRanges
+          int newStart = Math.min(oldEventTime.start(), newEventTime.start());
+          int newEnd = Math.max(oldEventTime.end(), newEventTime.end());
+          TimeRange newRange = TimeRange.fromStartEnd(newStart, newEnd, false);
+
+          // Replace old TimeRange with new block TimeRange
+          blockTimes.set(i, newRange);
+          blockTimeAdded = true;
+          // Ensures that it only merges with the first block TimeRange
+          break;
+        }
       }
-      if (timeRange.end() < end) {
-        end = timeRange.end();
+      // Add to end of list if a block was not added
+      if (!blockTimeAdded) {
+        blockTimes.add(time);
       }
     }
-    return TimeRange.fromStartEnd(start, end, true);
+    return blockTimes;
   }
 
+  /** Returns the inverse of TimeRanges from blockTimes. */
+  private ArrayList<TimeRange> invertTimeRanges(ArrayList<TimeRange> blockTimes) {
+    ArrayList<TimeRange> invertedTimeRanges = new ArrayList<>();
+    int lastEndTime = TimeRange.START_OF_DAY;
+    // Creates new TimeRanges that have the same start time as the previous end time
+    // and the same end time as the next start time, or the end of the day
+    for (TimeRange blockTime : blockTimes) {
+      int start = lastEndTime;
+      int end = blockTime.start();
+      invertedTimeRanges.add(TimeRange.fromStartEnd(start, end, false));
+      lastEndTime = end + blockTime.duration();
+    }
+    invertedTimeRanges.add(TimeRange.fromStartEnd(lastEndTime, TimeRange.END_OF_DAY, true));
+    return invertedTimeRanges;
+  }
 
+  /** Returns TimeRanges with a duration greater than or equal to the specified duration. */
+  private ArrayList<TimeRange> getTimesLongerThan(long duration, ArrayList<TimeRange> timeRanges){
+    ArrayList<TimeRange> longTimeRanges = new ArrayList<>();
+    for (TimeRange range : timeRanges) {
+      if (range.duration() >= duration) {
+        longTimeRanges.add(range);
+      }
+    }
+    return longTimeRanges;
+  }
 }
